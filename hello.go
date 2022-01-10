80c65e0b4c7245c4684a4c40ac272e2ab3c041b0
liorf <lior.franko@ironesrc.com>
2022-01-10 18:19:49 +0200

    working patch

--- hello.go
+++ hello.go
@@ -13,28 +13,12 @@ import (
 	"github.com/kelseyhightower/envconfig"
 	"github.com/liorfranko/configmap-attacher/options"
 	log "github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
-
-type Patch struct {
-	Metadata Metadata
-}
-
-type Metadata struct {
-	Labels          map[string]string `json:"labels"`
-	ownerReferences []ownerReferences `json:"ownerReferences"`
-}
-type ownerReferences struct {
-	apiVersion         string `json:"apiVersion"`
-	blockOwnerDeletion bool   `json:"blockOwnerDeletion"`
-	controller         bool   `json:"controller"`
-	kind               string `json:"kind"`
-	name               string `json:"name"`
-	uid                string `json:"uid"`
-}
 
 func Hello() string {
 	return "Hello, world"
@@ -117,63 +101,81 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// configmap, err := clientset.CoreV1().ConfigMaps(*namespacePtr).Get(context.Background(), *configMapPtr, metav1.GetOptions{})
-	// if err != nil {
-	// 	log.Fatalln("failed to get configmap:", err)
-	// }
-
-	// // print configmaps
-	// log.Println("printing configmpas", configmap)
-
-	// x := runCmd("-n", *namespacePtr, "get", "rollout", *rolloutPtr, "-o", "json")
-	// var newRs string
-	// var uid string
-	// if val, ok := x["status"]; ok {
-	// 	v := val.(map[string]interface{})
-	// 	if val2, ok2 := v["currentPodHash"]; ok2 {
-	// 		newRs = fmt.Sprintf("%v", val2)
-	// 	} else {
-	// 		log.Fatal("currentPodHash was not found in rollout status")
-	// 	}
-	// } else {
-	// 	log.Fatal("status was not found in rollout object")
-	// }
-	// x = runCmd("-n", *namespacePtr, "get", "replicasets.apps", *rolloutPtr+"-"+newRs, "-o", "json")
-	// if val, ok := x["metadata"]; ok {
-	// 	v := val.(map[string]interface{})
-	// 	if val2, ok2 := v["uid"]; ok2 {
-	// 		uid = fmt.Sprintf("%v", val2)
-	// 	} else {
-	// 		log.Fatal("uid was not found in metadata.replicaset object")
-	// 	}
-	// } else {
-	// 	log.Fatal("metadata was not found in replicaset object")
-	// }
-	// fmt.Println(newRs)
-	// fmt.Println(uid)
-	newRs := "7796f4cd8f"
-	uid := "07a80790-f2fc-46a3-8598-3d7bf110c143"
-	fmt.Println(newRs)
-	fmt.Println(uid)
-	c := ownerReferences{
-		apiVersion:         "apps/v1",
-		blockOwnerDeletion: true,
-		controller:         true,
-		kind:               "ReplicaSet",
-		name:               (*rolloutPtr + "-" + newRs),
-		uid:                uid,
+	configmap, err := clientset.CoreV1().ConfigMaps(*namespacePtr).Get(context.Background(), *configMapPtr, metav1.GetOptions{})
+	if err != nil {
+		log.Fatalln("failed to get configmap:", err)
 	}
-	fmt.Println(c)
-	patch3 := struct {
-		Metadata struct {
-			ownerReferences [1]ownerReferences `json:"ownerReferences"`
-		} `json:"metadata"`
-	}{}
-	patch3.Metadata.ownerReferences[0] = c
-	patchJson, _ := json.Marshal(patch3)
-	out, err2 := clientset.CoreV1().ConfigMaps(*namespacePtr).Patch(context.Background(), *configMapPtr, types.MergePatchType, patchJson, v1.PatchOptions{})
+
+	// print configmaps
+	log.Println("printing configmpas", configmap)
+
+	x := runCmd("-n", *namespacePtr, "get", "rollout", *rolloutPtr, "-o", "json")
+	var newRs string
+	var uid string
+	if val, ok := x["status"]; ok {
+		v := val.(map[string]interface{})
+		if val2, ok2 := v["currentPodHash"]; ok2 {
+			newRs = fmt.Sprintf("%v", val2)
+		} else {
+			log.Fatal("currentPodHash was not found in rollout status")
+		}
+	} else {
+		log.Fatal("status was not found in rollout object")
+	}
+	x = runCmd("-n", *namespacePtr, "get", "replicasets.apps", *rolloutPtr+"-"+newRs, "-o", "json")
+	if val, ok := x["metadata"]; ok {
+		v := val.(map[string]interface{})
+		if val2, ok2 := v["uid"]; ok2 {
+			uid = fmt.Sprintf("%v", val2)
+		} else {
+			log.Fatal("uid was not found in metadata.replicaset object")
+		}
+	} else {
+		log.Fatal("metadata was not found in replicaset object")
+	}
+	fmt.Println(uid)
+	// type ownerReferences struct {
+	// 	apiVersion         string `json:"apiVersion"`
+	// 	blockOwnerDeletion bool   `json:"blockOwnerDeletion"`
+	// 	controller         bool   `json:"controller"`
+	// 	kind               string `json:"kind"`
+	// 	name               string `json:"name"`
+	// 	uid                string `json:"uid"`
+	// }
+	// type Metadata struct {
+	// 	Labels          map[string]interface{} `json:"labels"`
+	// 	ownerReferences []ownerReferences      `json:"ownerReferences"`
+	// }
+	// patch3 := Metadata{
+	// 	Labels: {"dsds": "dsdsds"},
+	// 	ownerReferences: []ownerReferences{
+	// 		c,
+	// 	},
+	// }
+
+	// patch3.Metadata.Labels = map[string]string{}
+	// patch3.Metadata.Labels["hi"] = ""
+	// patch3.Metadata.ownerReferences[0] = {
+	// 	"fdfd":"fdfd",
+	// 	"sdsds":"dsdsdgh"
+	// }
+	// patch3.Metadata.ownerReferences[0] = ownerReferences{
+	// 	apiVersion:         "apps/v1",
+	// 	blockOwnerDeletion: true,
+	// 	controller:         true,
+	// 	kind:               "ReplicaSet",
+	// 	name:               "sleep-264446269",
+	// 	uid:                "6c078900-71f8-4bf5-8a7d-063a72c7a12b",
+	// }
+	patch := fmt.Sprintf(`{"metadata":{"ownerReferences":[{"apiVersion":"apps/v1","blockOwnerDeletion":true,"controller":true,"kind":"ReplicaSet","name":"%s-%s","uid":"%s"}]}}`, *rolloutPtr, newRs, uid)
+	patch2 := []byte(`{"metadata":{"ownerReferences":[{"apiVersion":"apps/v1","blockOwnerDeletion":true,"controller":true,"kind":"ReplicaSet","name":"sleep-7796f4cd8f","uid":"07a80790-f2fc-46a3-8598-3d7bf110c143"}]}}}`)
+	fmt.Println([]byte(patch))
+	fmt.Println(patch2)
+	patchJson, _ := json.Marshal(patch2)
+	fmt.Println(patchJson)
+	out, err2 := clientset.CoreV1().ConfigMaps(*namespacePtr).Patch(context.Background(), *configMapPtr, types.MergePatchType, []byte(patch), v1.PatchOptions{})
 	if err2 != nil {
 		panic(err2.Error())
 	}
-	fmt.Println(out)
+	log.Infof("Configmap %s has been patched, output: %s ", *configMapPtr, out)
 }
