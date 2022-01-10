20d13d9eff7b9f4a35b8ad472e2d4c7d86875975
liorf <lior.franko@ironesrc.com>
2022-01-10 18:40:55 +0200

    Create Runner func

--- hello.go
+++ hello.go
@@ -20,10 +20,6 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func Hello() string {
-	return "Hello, world"
-}
-
 func runCmd(str ...string) map[string]interface{} {
 	cmd := exec.Command("kubectl", str...)
 	log.Infof("Running kubectl command: '%v'", cmd)
@@ -50,16 +46,68 @@ func runCmd(str ...string) map[string]interface{} {
 	return x
 
 }
-func main() {
-	fmt.Println(Hello())
+func Runner(configMapPtr string, rolloutPtr string, namespacePtr string) {
+	// Bootstrap k8s configuration from local 	Kubernetes config file
+	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	log.Println("Using kubeconfig file: ", kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// Create an rest client not targeting specific API version
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	configmap, err := clientset.CoreV1().ConfigMaps(namespacePtr).Get(context.Background(), configMapPtr, metav1.GetOptions{})
+	if err != nil {
+		log.Fatalln("failed to get configmap:", err)
+	}
+
+	// print configmaps
+	log.Println("printing configmpas", configmap)
+
+	x := runCmd("-n", namespacePtr, "get", "rollout", rolloutPtr, "-o", "json")
+	var newRs string
+	var uid string
+	if val, ok := x["status"]; ok {
+		v := val.(map[string]interface{})
+		if val2, ok2 := v["currentPodHash"]; ok2 {
+			newRs = fmt.Sprintf("%v", val2)
+		} else {
+			log.Fatal("currentPodHash was not found in rollout status")
+		}
+	} else {
+		log.Fatal("status was not found in rollout object")
+	}
+	x = runCmd("-n", namespacePtr, "get", "replicasets.apps", rolloutPtr+"-"+newRs, "-o", "json")
+	if val, ok := x["metadata"]; ok {
+		v := val.(map[string]interface{})
+		if val2, ok2 := v["uid"]; ok2 {
+			uid = fmt.Sprintf("%v", val2)
+		} else {
+			log.Fatal("uid was not found in metadata.replicaset object")
+		}
+	} else {
+		log.Fatal("metadata was not found in replicaset object")
+	}
+	patch := fmt.Sprintf(`{"metadata":{"ownerReferences":[{"apiVersion":"apps/v1","blockOwnerDeletion":true,"controller":true,"kind":"ReplicaSet","name":"%s-%s","uid":"%s"}]}}`, rolloutPtr, newRs, uid)
+	out, err := clientset.CoreV1().ConfigMaps(namespacePtr).Patch(context.Background(), configMapPtr, types.MergePatchType, []byte(patch), v1.PatchOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	log.Infof("Configmap %s has been patched, output: %s ", configMapPtr, out)
+}
+func main() {
+	// Set and parse CLI options
 	configMapPtr := flag.String("configmap", "", "Configmap to add the ownerReference")
 	rolloutPtr := flag.String("rollout", "", "Rollout that will be the ownerReference")
 	namespacePtr := flag.String("namespace", "", "The namespace of the rollout and configmap")
-	dryRunPtr := flag.Bool("dryRun", false, "Run in dry-run mode")
 	flag.Parse()
 
-	fmt.Printf("configMapPtr: %s, rolloutPtr: %s, namespacePtr: %s, dryRunPtr: %t\n", *configMapPtr, *rolloutPtr, *namespacePtr, *dryRunPtr)
+	fmt.Printf("configMapPtr: %s, rolloutPtr: %s, namespacePtr: %s\n", *configMapPtr, *rolloutPtr, *namespacePtr)
 	if *configMapPtr == "" || *rolloutPtr == "" || *namespacePtr == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
@@ -86,96 +134,6 @@ func main() {
 	// Start configmap-attacher
 	log.Infof("Starting configmap-attacher v%v", opts.Version)
 	log.Infof("Configmap-attacher variables, configmap: %s, rollout: %s, namespace: %s", *configMapPtr, *rolloutPtr, *namespacePtr)
+	Runner(*configMapPtr, *rolloutPtr, *namespacePtr)
 
-	// // Bootstrap k8s configuration from local 	Kubernetes config file
-	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-	log.Println("Using kubeconfig file: ", kubeconfig)
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create an rest client not targeting specific API version
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	configmap, err := clientset.CoreV1().ConfigMaps(*namespacePtr).Get(context.Background(), *configMapPtr, metav1.GetOptions{})
-	if err != nil {
-		log.Fatalln("failed to get configmap:", err)
-	}
-
-	// print configmaps
-	log.Println("printing configmpas", configmap)
-
-	x := runCmd("-n", *namespacePtr, "get", "rollout", *rolloutPtr, "-o", "json")
-	var newRs string
-	var uid string
-	if val, ok := x["status"]; ok {
-		v := val.(map[string]interface{})
-		if val2, ok2 := v["currentPodHash"]; ok2 {
-			newRs = fmt.Sprintf("%v", val2)
-		} else {
-			log.Fatal("currentPodHash was not found in rollout status")
-		}
-	} else {
-		log.Fatal("status was not found in rollout object")
-	}
-	x = runCmd("-n", *namespacePtr, "get", "replicasets.apps", *rolloutPtr+"-"+newRs, "-o", "json")
-	if val, ok := x["metadata"]; ok {
-		v := val.(map[string]interface{})
-		if val2, ok2 := v["uid"]; ok2 {
-			uid = fmt.Sprintf("%v", val2)
-		} else {
-			log.Fatal("uid was not found in metadata.replicaset object")
-		}
-	} else {
-		log.Fatal("metadata was not found in replicaset object")
-	}
-	fmt.Println(uid)
-	// type ownerReferences struct {
-	// 	apiVersion         string `json:"apiVersion"`
-	// 	blockOwnerDeletion bool   `json:"blockOwnerDeletion"`
-	// 	controller         bool   `json:"controller"`
-	// 	kind               string `json:"kind"`
-	// 	name               string `json:"name"`
-	// 	uid                string `json:"uid"`
-	// }
-	// type Metadata struct {
-	// 	Labels          map[string]interface{} `json:"labels"`
-	// 	ownerReferences []ownerReferences      `json:"ownerReferences"`
-	// }
-	// patch3 := Metadata{
-	// 	Labels: {"dsds": "dsdsds"},
-	// 	ownerReferences: []ownerReferences{
-	// 		c,
-	// 	},
-	// }
-
-	// patch3.Metadata.Labels = map[string]string{}
-	// patch3.Metadata.Labels["hi"] = ""
-	// patch3.Metadata.ownerReferences[0] = {
-	// 	"fdfd":"fdfd",
-	// 	"sdsds":"dsdsdgh"
-	// }
-	// patch3.Metadata.ownerReferences[0] = ownerReferences{
-	// 	apiVersion:         "apps/v1",
-	// 	blockOwnerDeletion: true,
-	// 	controller:         true,
-	// 	kind:               "ReplicaSet",
-	// 	name:               "sleep-264446269",
-	// 	uid:                "6c078900-71f8-4bf5-8a7d-063a72c7a12b",
-	// }
-	patch := fmt.Sprintf(`{"metadata":{"ownerReferences":[{"apiVersion":"apps/v1","blockOwnerDeletion":true,"controller":true,"kind":"ReplicaSet","name":"%s-%s","uid":"%s"}]}}`, *rolloutPtr, newRs, uid)
-	patch2 := []byte(`{"metadata":{"ownerReferences":[{"apiVersion":"apps/v1","blockOwnerDeletion":true,"controller":true,"kind":"ReplicaSet","name":"sleep-7796f4cd8f","uid":"07a80790-f2fc-46a3-8598-3d7bf110c143"}]}}}`)
-	fmt.Println([]byte(patch))
-	fmt.Println(patch2)
-	patchJson, _ := json.Marshal(patch2)
-	fmt.Println(patchJson)
-	out, err2 := clientset.CoreV1().ConfigMaps(*namespacePtr).Patch(context.Background(), *configMapPtr, types.MergePatchType, []byte(patch), v1.PatchOptions{})
-	if err2 != nil {
-		panic(err2.Error())
-	}
-	log.Infof("Configmap %s has been patched, output: %s ", *configMapPtr, out)
 }
